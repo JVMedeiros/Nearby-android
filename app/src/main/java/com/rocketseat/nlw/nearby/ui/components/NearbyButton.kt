ed686c04bbdad7aee5f05482c5e3bdd3ba913918
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-12-11 17:25:03 -0300

    Feat: NearbyButton component

--- app/src/main/java/com/rocketseat/nlw/nearby/ui/components/NearbyButton.kt
+++ app/src/main/java/com/rocketseat/nlw/nearby/ui/components/NearbyButton.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.min
 import com.rocketseat.nlw.nearby.R
 import com.rocketseat.nlw.nearby.ui.theme.GreenBase
 import com.rocketseat.nlw.nearby.ui.theme.Typography
