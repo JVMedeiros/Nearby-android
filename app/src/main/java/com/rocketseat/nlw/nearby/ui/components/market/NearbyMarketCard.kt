493645cbc1a83167cd2096b7d285129e95fd0d0b
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-12-11 23:04:54 -0300

    Refactor: renaming class

---
+++ app/src/main/java/com/rocketseat/nlw/nearby/ui/components/market/NearbyMarketCard.kt
@@ -0,0 +1,14 @@
+package com.rocketseat.nlw.nearby.ui.components.market
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.rocketseat.nlw.nearby.data.model.Market
+
+@Composable
+fun NearbyMarketCard(
+    modifier: Modifier = Modifier,
+    maket: Market,
+    onClick: (Market) -> Unit
+) {
+
+}
