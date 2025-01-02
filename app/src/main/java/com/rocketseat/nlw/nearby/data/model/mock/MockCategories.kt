cc0bdcf25740a661c9c43b88a112ba89e66fe575
João Medeiros <jv.medeiros.gallina@gmail.com>
2025-01-02 20:33:35 -0300

    Feat: MockCategories file add

---
+++ app/src/main/java/com/rocketseat/nlw/nearby/data/model/mock/MockCategories.kt
@@ -0,0 +1,22 @@
+package com.rocketseat.nlw.nearby.data.model.mock
+
+import com.rocketseat.nlw.nearby.data.model.Category
+
+val mockCatetories = listOf(
+    Category(
+        id = "1",
+        name = "Alimentação"
+    ),
+    Category(
+        id = "2",
+        name = "Entretenimento"
+    ),
+    Category(
+        id = "3",
+        name = "Farmácia"
+    ),
+    Category(
+        id = "4",
+        name = "Supermercado"
+    )
+)
