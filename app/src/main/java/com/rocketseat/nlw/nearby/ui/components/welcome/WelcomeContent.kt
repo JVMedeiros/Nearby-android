cdb7c848a14531e82e968eaa65ad17d73e9b01cc
João Medeiros <jv.medeiros.gallina@gmail.com>
2025-01-02 21:29:55 -0300

    Refactor: Implementing WelcomeHowItWorksTip component

--- app/src/main/java/com/rocketseat/nlw/nearby/ui/components/welcome/WelcomeContent.kt
+++ app/src/main/java/com/rocketseat/nlw/nearby/ui/components/welcome/WelcomeContent.kt
@@ -8,7 +8,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.rocketseat.nlw.nearby.R
-import com.rocketseat.nlw.nearby.ui.screen.WelcomeHowItWorksTip
 import com.rocketseat.nlw.nearby.ui.theme.Typography
 
 @Composable
