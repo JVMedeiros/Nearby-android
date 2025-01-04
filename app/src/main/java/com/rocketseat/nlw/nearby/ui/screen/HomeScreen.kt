7f32ffa632bcd0bc96204aa9775ee5404c7f46bf
João Medeiros <jv.medeiros.gallina@gmail.com>
2025-01-04 00:49:11 -0300

    Feat: Implementing Navigation between screens

--- app/src/main/java/com/rocketseat/nlw/nearby/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/rocketseat/nlw/nearby/ui/screen/HomeScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.maps.android.compose.GoogleMap
+import com.rocketseat.nlw.nearby.data.model.Market
 import com.rocketseat.nlw.nearby.data.model.mock.mockCatetories
 import com.rocketseat.nlw.nearby.data.model.mock.mockMarkets
 import com.rocketseat.nlw.nearby.ui.components.category.NearbyCategoryFilterChipList
@@ -27,7 +28,7 @@ import com.rocketseat.nlw.nearby.ui.theme.Gray100
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
+fun HomeScreen(modifier: Modifier = Modifier, onNavigateToMarketDetails: (Market) -> Unit) {
     val bottomSheetState = rememberBottomSheetScaffoldState()
     var isBottomSheetOpened by remember { mutableStateOf(true) }
 
@@ -46,7 +47,9 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                         .fillMaxSize()
                         .padding(16.dp),
                     markets = mockMarkets,
-                    onMarketClick = {}
+                    onMarketClick = { selectedMarket ->
+                        onNavigateToMarketDetails(selectedMarket)
+                    }
                 )
             },
             content = {
@@ -77,5 +80,5 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 private fun HomeScreenPreview() {
-    HomeScreen()
+    HomeScreen(onNavigateToMarketDetails = {})
 }
